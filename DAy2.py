32c1e61245f960528769cd70ae9b2bd78787f905
LakshyaSoni4151 <lakshya.soni952@gmail.com>
2026-05-30 10:08:45 +0530

    Add files via upload

---
+++ DAy2.py
@@ -0,0 +1,123 @@
+#a=10
+#print(a)
+
+#variable
+#a=10
+#print(a)
+#print(id(a))
+#b=10
+#print(b)
+#print(id(b))
+
+
+#integer
+#a=10
+#print(a)
+#print (type(a))
+
+#
+#num=input("Enter the Number")
+#print(num)
+#print(num+str(10))
+#print(num * 10)
+
+#Arithmetic operator
+#print(10+20)
+#a=10
+#b=15
+#print (a+b)
+
+#division
+
+#num_1 =20
+#num_2 =2
+#print (num_1/num_2)
+
+#//
+#num_1 =20
+#num_2 =2
+#print (num_1//num_2)
+
+#%
+#num_1 =20
+#num_2 =2
+#print (num_1%num_2)
+
+#**
+#num_1 =5
+#num_2 = 3
+#print (num_1**num_2)
+#odd even
+#num1 = int(input("Enter num1:"))
+#num2 = 2
+#print (num1 % num2)
+
+#relational operator
+#num_1 =5
+#num_2 = 3
+#print (num_1 == num_2)
+
+#<
+#num_1 =5
+#um_2 = 3
+#print (num_1 < num_2)
+
+
+#>
+#num_1 =5
+#num_2 = 3
+#print (num_1 > num_2)
+
+#<=
+#num_1 =5
+#num_2 = 5
+#print (num_1 <= num_2)
+
+#>=
+#num_1 =5
+#num_2 = 6
+#print (num_1 >= num_2)
+
+#
+#num_1 =5
+#num_2 = 6
+#print (num_1 >= num_2)
+
+#!=
+#num_1 =5
+#num_2 = 6
+#print (num_1 != num_2)
+
+
+#+=
+#a = 10
+#a+=20
+#print(a)
+
+#a-=
+#a = 10
+#a-=20
+#print(a)
+
+#a//=
+a = 10
+a//=20
+print(a)
+
+#logical operator
+
+#a=10
+#b=20
+#print (a and b)
+
+#a=10
+#b=20
+#print (a and b)
+#print (a or b)
+
+#name1 ="hello"
+#name2 =""
+#print (name1 and name2)
+#print (name1 or name2)
+
+
