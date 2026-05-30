5ef4b4a7fee99efbb597b5c210ef482a41032753
LakshyaSoni4151 <lakshya.soni952@gmail.com>
2026-05-30 11:27:31 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,28 @@
+#import module
+# import math
+
+# print(math.factorial(4))
+# print(math.pow(5,2))
+# print(math.pi)
+# print(math.floor(10.7234))
+# print(math.ceil(10.0123))
+
+#example
+# import math as Lakshya
+
+# print(Lakshya.factorial(4))
+# print(Lakshya.pow(5,2))
+# print(Lakshya.pi)
+# print(Lakshya.floor(10.7234))
+# print(Lakshya.ceil(10.0123))
+
+#exammple2
+# from math import factorial,pi
+# print(factorial(5))
+# print(pi)
+
+#example3
+# from math import*
+# print(pow(5,3))
+# print(pi)
+# print(floor(10.3211))
