c6c059a9c5a0b1b87c0b6e9d4dbd812ce8874d1e
LakshyaSoni4151 <lakshya.soni952@gmail.com>
2026-06-03 11:25:25 +0530

    Add files via upload

---
+++ Day9.py
@@ -0,0 +1,32 @@
+#import numpy
+# import numpy as np
+# from numpy import arange
+# from numpy import *
+# n = numpy.arange(11)
+# print(type(n))
+# print(n)
+# print(n[0])
+
+#example 1d
+# n = numpy.arange(12)
+# d = n.reshape(3,4) #3 -> rows or 4 -> cols
+# print(type(d))
+# print(d)
+
+
+#example 2d
+# n= numpy.arange(12).reshape(4,3)
+# print(n)
+
+#example 3d
+
+
+# n = numpy.arange(24).reshape(2,3,4)
+# print(n)
+
+#EXAMPLE split #2,5,8,11 ->print
+# n = numpy.arange(12).reshape(4,3)
+# print(n)
+# print(n[0:4:,2]) #first row or second cols
+# print(n[0:4,2:4]) #first row or second cols
+# print(n[1:3,1]) #first row or second cols
