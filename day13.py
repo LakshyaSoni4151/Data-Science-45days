ea54704ae4a343b610416fe1f8417311a93d21a3
LakshyaSoni4151 <lakshya.soni952@gmail.com>
2026-06-11 09:16:43 +0530

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,26 @@
+#PANDA
+# import pandas as pd
+# df = pd.Series([10,20,30])
+# print(df)
+# print(df[0])
+
+#dictionary
+# import pandas as pd
+
+# dict = {"name":["dheeraj","kunal","praveen","anjali","abhishek singh"],
+#         "age":[20,21,22,23,24],
+#         "salary":[{"In-hand":"15000","ctc":"1.2lpa"},{"In-hand":"20000","ctc":"2.2lpa"},
+#                   {"In-hand":"25000","ctc":"3.2lpa"},{"In-hand":"30000","ctc":"3.2lpa"},
+#                   {"In-hand":"40000","ctc":"4.2lpa"}
+#                  ]
+#        }
+
+# df = pd.DataFrame(data=dict)
+# print(df)
+
+#csv  
+# # csv
+# import pandas as pd
+# df = pd.read_csv("file1.csv")
+# print(df)
+
